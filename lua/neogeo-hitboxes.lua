60373b96c493e4e5a99e5b018c138afc566d315c
dammit9x@hotmail.com <dammit9x@hotmail.com@b55800c6-aee7-1b21-8cfc-8272c5ace2c0>
2011-03-08 08:47:50 +0000

    neogeo hitboxes: * added kof94 * added autoguard boxes

--- lua/neogeo-hitboxes.lua
+++ lua/neogeo-hitboxes.lua
@@ -1,5 +1,5 @@
 print("NeoGeo hitbox viewer")
-print("March 7, 2011")
+print("March 8, 2011")
 print("http://code.google.com/p/mame-rr/")
 print("Lua hotkey 1: toggle blank screen")
 print("Lua hotkey 2: toggle object axis")
@@ -9,11 +9,11 @@ print("Lua hotkey 5: toggle throwboxes") print()
 
 local VULNERABILITY_COLOR      = 0x7777FF40
 local ATTACK_COLOR             = 0xFF000060
-local PUSH_COLOR               = 0x00FF0040
-local THROW_COLOR              = 0xFFFF0060
-local THROWABLE_COLOR          = 0xFFFFFF00
 local PROJ_VULNERABILITY_COLOR = 0x77CCFF40
 local PROJ_ATTACK_COLOR        = 0xFF66FF60
+local PUSH_COLOR               = 0x00FF0040
+local AUTOGUARD_COLOR          = 0xCCCCCC60
+local THROW_COLOR              = 0xFFFF0060
 local AXIS_COLOR               = 0xFFFFFFFF
 local BLANK_COLOR              = 0xFFFFFFFF
 local AXIS_SIZE                = 16
@@ -28,96 +28,104 @@ local DRAW_THROWBOXES          = true
 local GAME_PHASE_NOT_PLAYING = 0
 local VULNERABILITY_BOX      = 1
 local ATTACK_BOX             = 2
-local PUSH_BOX               = 3
-local THROW_BOX              = 4
-local THROWABLE_BOX          = 5
-local PROJ_VULNERABILITY_BOX = 6
-local PROJ_ATTACK_BOX        = 7
+local PROJ_VULNERABILITY_BOX = 3
+local PROJ_ATTACK_BOX        = 4
+local PUSH_BOX               = 5
+local AUTOGUARD_BOX          = 6
+local THROW_BOX              = 7
 
 local fill = {
 	VULNERABILITY_COLOR,
 	ATTACK_COLOR,
-	PUSH_COLOR,
-	THROW_COLOR,
-	THROWABLE_COLOR,
 	PROJ_VULNERABILITY_COLOR,
 	PROJ_ATTACK_COLOR,
+	PUSH_COLOR,
+	AUTOGUARD_COLOR,
+	THROW_COLOR,
 }
 
 local outline = {
 	bit.bor(0xFF, VULNERABILITY_COLOR),
 	bit.bor(0xFF, ATTACK_COLOR),
-	bit.bor(0xC0, PUSH_COLOR),
-	bit.bor(0xFF, THROW_COLOR),
-	bit.bor(0xE0, THROWABLE_COLOR),
 	bit.bor(0xFF, PROJ_VULNERABILITY_COLOR),
 	bit.bor(0xFF, PROJ_ATTACK_COLOR),
+	bit.bor(0xC0, PUSH_COLOR),
+	bit.bor(0xC0, AUTOGUARD_COLOR),
+	bit.bor(0xFF, THROW_COLOR),
 }
 
 local profile = {
 	{
+		games = {"kof94"},
+		address = {
+			player           = 0x108100,
+			game_phase       = 0x1090B3,
+			left_screen_edge = 0x10904C,
+			top_screen_edge  = 0x10905C,
+			obj_ptr_list     = 0x1097A6,
+		},
+		offset = {status = 0x7A},
+	},
+	{
 		games = {"kof95"},
-		game_phase = 0x10B088,
+		address = {game_phase = 0x10B088},
 	},
 	{
 		games = {"kof96"},
-		game_phase = 0x10B08E,
+		address = {game_phase = 0x10B08E},
 	},
 	{
 		games = {"kof97"},
-		game_phase = 0x10B092,
+		address = {game_phase = 0x10B092},
 	},
 	{
 		games = {"kof98"},
-		game_phase = 0x10B094,
+		address = {game_phase = 0x10B094},
 	},
 	{
 		games = {"kof99", "kof2000"},
-		game_phase = 0x10B048,
+		address = {game_phase = 0x10B048},
 	},
 	{
 		games = {"kof2001", "kof2002"},
-		game_phase = 0x10B056,
+		address = {game_phase = 0x10B056},
 	},
 }
 
 for game in ipairs(profile) do
 	local g = profile[game]
-	g.address = g.address or {
-		player           = 0x108100,
-		game_phase       = g.game_phase,
-		left_screen_edge = g.game_phase + 0x038,
-		top_screen_edge  = g.game_phase + 0x040,
-		obj_ptr_list     = g.game_phase + 0xE90,
-	}
-	g.offset = g.offset or {
-		player_space     = 0x200,
-		x_position       = 0x18,
-		--y_position       = 0x20,
-		y_position       = 0x26,
-		facing_dir       = 0x31,
-		status           = 0x7C,
-		invulnerability  = {},
-		unpushability    = {},
+	g.nplayers     = g.nplayers or 2
+	g.ground_level = g.ground_level or 16
+	g.address.player           = g.address.player or 0x108100
+	g.address.left_screen_edge = g.address.left_screen_edge or g.address.game_phase + 0x038
+	g.address.top_screen_edge  = g.address.top_screen_edge or g.address.game_phase + 0x040
+	g.address.obj_ptr_list     = g.address.obj_ptr_list or g.address.game_phase + 0xE90
+	g.offset = g.offset or {}
+	g.offset.player_space    = g.offset.player_space or 0x200
+	g.offset.x_position      = g.offset.x_position or 0x18
+	g.offset.y_position      = g.offset.y_position or 0x26
+	g.offset.facing_dir      = g.offset.facing_dir or 0x31
+	g.offset.status          = g.offset.status or 0x7C
+	g.offset.invulnerability = g.offset.invulnerability or {}
+	g.offset.unpushability   = g.offset.unpushability or {}
+	g.box = g.box or {
+		radius_read = memory.readbyte,
+		offset_read = memory.readbytesigned,
 		hval = 0x1, vval = 0x2, hrad = 0x3, vrad = 0x4,
 	}
-	g.boxes = g.boxes or {
+	g.box_list = g.box_list or {
 		{offset = 0xA4, type = PUSH_BOX},
 		{offset = 0x95, active_bit = 1, type = VULNERABILITY_BOX},
 		{offset = 0x9A, active_bit = 2, type = VULNERABILITY_BOX},
 		{offset = 0x9F, active_bit = 3, type = VULNERABILITY_BOX},
 		{offset = 0x90, active_bit = 0, type = ATTACK_BOX},
 	}
-	g.nplayers        = g.nplayers or 2
-	g.ground_level    = g.ground_level or 16
-	g.box_radius_read = g.box_radius_read or memory.readbyte
-	g.box_offset_read = g.box_radius_read == memory.readbyte and memory.readbytesigned or memory.readwordsigned
-	for _, box in ipairs(g.boxes) do
+	for _, box in ipairs(g.box_list) do
 		box.active = box.active_bit and bit.lshift(1, box.active_bit)
 	end
 end
 
-local game, prepare_boxes
+local game
 local globals = {
 	game_phase       = 0,
 	left_screen_edge = 0,
@@ -179,44 +187,54 @@ end
 
 
 local function define_box(obj, entry)
-	if game.boxes[entry].type == PUSH_BOX then
-		if memory.readbytesigned(obj.base + game.boxes[entry].offset) <= 0 then
+	local box_type = game.box_list[entry].type
+
+	if box_type == PUSH_BOX then
+		if memory.readbytesigned(obj.base + game.box_list[entry].offset) < 0 then
 			return nil
 		end
-	elseif game.boxes[entry].active and bit.band(obj.status, game.boxes[entry].active) == 0 then
+	elseif game.box_list[entry].active and bit.band(obj.status, game.box_list[entry].active) == 0 then
 		return nil
 	end
 
-	local address = obj.base + game.boxes[entry].offset
-	local hval = game.box_offset_read(address + game.offset.hval)
-	local vval = game.box_offset_read(address + game.offset.vval)
-	local hrad = game.box_radius_read(address + game.offset.hrad)
-	local vrad = game.box_radius_read(address + game.offset.vrad)
-	hval = hval * (bit.band(obj.facing_dir, 1) > 0 and -1 or 1)
+	local address = obj.base + game.box_list[entry].offset
+	local id = memory.readbyte(address)
+	for _, value in ipairs({0x2,0x3,0x4,0x9,0xA}) do
+		if id == value then
+			box_type = VULNERABILITY_BOX
+			break
+		end
+	end
+	if id == 0xB then
+		box_type = AUTOGUARD_BOX
+	end
 
-	local box_type = game.boxes[entry].type
-	if obj.projectile and box_type == VULNERABILITY_BOX then
-		box_type = PROJ_VULNERABILITY_BOX
-	elseif obj.projectile and box_type == ATTACK_BOX then
-		box_type = PROJ_ATTACK_BOX
-	elseif box_type == ATTACK_BOX then
-		local id = memory.readbyte(address)
-		for _, value in ipairs({0x2,0x3,0x4,0x9,0xA}) do
-			if id == value then
-				box_type = VULNERABILITY_BOX
-				break
-			end
+	if obj.projectile then
+		if box_type == VULNERABILITY_BOX then
+			box_type = PROJ_VULNERABILITY_BOX
+		elseif box_type == ATTACK_BOX then
+			box_type = PROJ_ATTACK_BOX
+		else
+			return nil
 		end
 	end
 
+	local hval = game.box.offset_read(address + game.box.hval)
+	local vval = game.box.offset_read(address + game.box.vval)
+	local hrad = game.box.radius_read(address + game.box.hrad)
+	local vrad = game.box.radius_read(address + game.box.vrad)
+	hval = obj.pos_x + hval * (bit.band(obj.facing_dir, 1) > 0 and -1 or 1)
+	vval = obj.pos_y + vval
+
 	return {
 		type   = box_type,
-		hval   = obj.pos_x + hval,
-		vval   = obj.pos_y + vval,
-		left   = obj.pos_x + hval - hrad,
-		right  = obj.pos_x + hval + hrad - 1,
-		top    = obj.pos_y + vval - vrad,
-		bottom = obj.pos_y + vval + vrad - 1,
+		id     = id,
+		pos_x  = hval,
+		pos_y  = vval,
+		left   = hval - hrad,
+		right  = hval + hrad - 1,
+		top    = vval - vrad,
+		bottom = vval + vrad - 1,
 	}
 end
 
@@ -247,7 +265,7 @@ local function add_object(address, projectile)
 	obj.pos_x = game_x_to_mame(memory.readwordsigned(obj.base + game.offset.x_position))
 	obj.pos_y = game_y_to_mame(memory.readwordsigned(obj.base + game.offset.y_position))
 	obj.facing_dir = bit.band(memory.readbyte(obj.base + game.offset.facing_dir), 1)
-	for entry in ipairs(game.boxes) do
+	for entry in ipairs(game.box_list) do
 		obj[entry] = define_box(obj, entry)
 	end
 	return obj
@@ -321,8 +339,9 @@ local function draw_hitbox(obj, entry)
 	end
 
 	if DRAW_MINI_AXIS then
-		gui.drawline(hb.hval, hb.vval-MINI_AXIS_SIZE, hb.hval, hb.vval+MINI_AXIS_SIZE, outline[hb.type])
-		gui.drawline(hb.hval-MINI_AXIS_SIZE, hb.vval, hb.hval+MINI_AXIS_SIZE, hb.vval, outline[hb.type])
+		gui.drawline(hb.pos_x, hb.pos_y-MINI_AXIS_SIZE, hb.pos_x, hb.pos_y+MINI_AXIS_SIZE, outline[hb.type])
+		gui.drawline(hb.pos_x-MINI_AXIS_SIZE, hb.pos_y, hb.pos_x+MINI_AXIS_SIZE, hb.pos_y, outline[hb.type])
+		gui.text(hb.pos_x, hb.pos_y, string.format("%02X", hb.id)) --debug
 	end
 
 	gui.box(hb.left, hb.top, hb.right, hb.bottom, fill[hb.type], outline[hb.type])
@@ -336,8 +355,8 @@ local function draw_axis(obj)
 	
 	gui.drawline(obj.pos_x, obj.pos_y-AXIS_SIZE, obj.pos_x, obj.pos_y+AXIS_SIZE, AXIS_COLOR)
 	gui.drawline(obj.pos_x-AXIS_SIZE, obj.pos_y, obj.pos_x+AXIS_SIZE, obj.pos_y, AXIS_COLOR)
-	gui.text(obj.pos_x, obj.pos_y, string.format("%06X",obj.base)) --debug
-	gui.text(obj.pos_x, obj.pos_y+8, string.format("%02X",obj.status)) --debug
+	gui.text(obj.pos_x, obj.pos_y, string.format("%06X", obj.base)) --debug
+	gui.text(obj.pos_x, obj.pos_y+8, string.format("%02X", obj.status)) --debug
 end
 
 
@@ -350,7 +369,7 @@ local function render_neogeo_hitboxes()
 		gui.box(0, 0, emu.screenwidth(), emu.screenheight(), BLANK_COLOR)
 	end
 
-	for entry in ipairs(game.boxes) do
+	for entry in ipairs(game.box_list) do
 		for _, obj in ipairs(frame_buffer[1]) do
 			if obj[entry] then
 				draw_hitbox(obj, entry)
