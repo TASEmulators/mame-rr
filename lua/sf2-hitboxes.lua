a662269e50950f6b550e475073ed88a171b87670
dammit9x@hotmail.com <dammit9x@hotmail.com@b55800c6-aee7-1b21-8cfc-8272c5ace2c0>
2011-05-18 06:57:42 +0000

    sf2 hitboxes: added throw & throwable boxes

--- lua/sf2-hitboxes.lua
+++ lua/sf2-hitboxes.lua
@@ -1,155 +1,167 @@
 print("Street Fighter II hitbox viewer")
-print("March 17, 2011")
+print("May 17, 2011")
 print("http://code.google.com/p/mame-rr/")
 print("Lua hotkey 1: toggle blank screen")
 print("Lua hotkey 2: toggle object axis")
 print("Lua hotkey 3: toggle hitbox axis")
-print("Lua hotkey 4: toggle pushboxes") print()
+print("Lua hotkey 4: toggle pushboxes")
+print("Lua hotkey 5: toggle throwable boxes")
+print()
 
-local VULNERABILITY_COLOR      = 0x7777FF40
-local ATTACK_COLOR             = 0xFF000040
-local PROJ_VULNERABILITY_COLOR = 0x00FFFF40
-local PROJ_ATTACK_COLOR        = 0xFF66FF60
-local PUSH_COLOR               = 0x00FF0020
-local WEAK_COLOR               = 0xFFFF0060
-local AXIS_COLOR               = 0xFFFFFFFF
-local BLANK_COLOR              = 0xFFFFFFFF
-local AXIS_SIZE                = 12
-local MINI_AXIS_SIZE           = 2
-local DRAW_DELAY               = 1
-local BLANK_SCREEN             = false
-local DRAW_AXIS                = false
-local DRAW_MINI_AXIS           = false
-local DRAW_PUSHBOXES           = true
-
-local GAME_PHASE_NOT_PLAYING = 0
-local NUMBER_OF_PLAYERS      = 2
-local MAX_GAME_PROJECTILES   = 8
-local MAX_BONUS_OBJECTS      = 16
-local VULNERABILITY_BOX      = 1
-local ATTACK_BOX             = 2
-local PROJ_VULNERABILITY_BOX = 3
-local PROJ_ATTACK_BOX        = 4
-local PUSH_BOX               = 5
-local WEAK_BOX               = 6
-
-local fill = {
-	VULNERABILITY_COLOR,
-	ATTACK_COLOR,
-	PROJ_VULNERABILITY_COLOR,
-	PROJ_ATTACK_COLOR,
-	PUSH_COLOR,
-	WEAK_COLOR,
+local boxes = {
+	      ["vulnerability"] = {color = 0x7777FF, fill = 0x40, outline = 0xFF},
+	             ["attack"] = {color = 0xFF0000, fill = 0x40, outline = 0xFF},
+	["proj. vulnerability"] = {color = 0x00FFFF, fill = 0x40, outline = 0xFF},
+	       ["proj. attack"] = {color = 0xFF66FF, fill = 0x40, outline = 0xFF},
+	               ["push"] = {color = 0x00FF00, fill = 0x20, outline = 0xFF},
+	               ["weak"] = {color = 0xFFFF00, fill = 0x40, outline = 0xFF},
+	              ["throw"] = {color = 0xFFFF00, fill = 0x40, outline = 0xFF},
+	          ["throwable"] = {color = 0xF0F0F0, fill = 0x20, outline = 0xFF},
+	      ["air throwable"] = {color = 0x202020, fill = 0x20, outline = 0xFF},
 }
 
-local outline = {
-	bit.bor(0xFF, VULNERABILITY_COLOR),
-	bit.bor(0xFF, ATTACK_COLOR),
-	bit.bor(0xFF, PROJ_VULNERABILITY_COLOR),
-	bit.bor(0xFF, PROJ_ATTACK_COLOR),
-	bit.bor(0xC0, PUSH_COLOR),
-	bit.bor(0xFF, WEAK_COLOR),
-}
+local AXIS_COLOR           = 0xFFFFFFFF
+local BLANK_COLOR          = 0xFFFFFFFF
+local AXIS_SIZE            = 12
+local MINI_AXIS_SIZE       = 2
+local BLANK_SCREEN         = false
+local DRAW_AXIS            = true
+local DRAW_MINI_AXIS       = false
+local DRAW_PUSHBOXES       = true
+local DRAW_THROWABLE_BOXES = false
+local DRAW_DELAY           = 1
+local NUMBER_OF_PLAYERS    = 2
+local MAX_GAME_PROJECTILES = 8
+local MAX_BONUS_OBJECTS    = 16
 
 local profile = {
 	{
 		games = {"sf2"},
+		status_type = "normal",
 		address = {
 			player           = 0xFF83C6,
 			projectile       = 0xFF938A,
 			left_screen_edge = 0xFF8BD8,
-			game_phase       = 0xFF83F7,
 		},
 		player_space       = 0x300,
 		box_parameter_size = 1,
 		box_list = {
-			{addr_table = 0xA, id_ptr = 0xD, id_space = 0x04, type = PUSH_BOX},
-			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x6, id_ptr = 0xB, id_space = 0x04, type = WEAK_BOX},
-			{addr_table = 0x8, id_ptr = 0xC, id_space = 0x0C, type = ATTACK_BOX},
+			{addr_table = 0xA, id_ptr = 0xD, id_space = 0x04, type = "push"},
+			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x6, id_ptr = 0xB, id_space = 0x04, type = "weak"},
+			{addr_table = 0x8, id_ptr = 0xC, id_space = 0x0C, type = "attack"},
 		},
+		throw_box_list = {
+			{param_offset = 0x6C, type = "throwable"},
+			{param_offset = 0x64, type = "throw"},
+		}
 	},
 	{
 		games = {"sf2ce","sf2hf"},
+		status_type = "normal",
 		address = {
 			player           = 0xFF83BE,
 			projectile       = 0xFF9376,
 			left_screen_edge = 0xFF8BC4,
-			game_phase       = 0xFF83EF,
 		},
 		player_space       = 0x300,
 		box_parameter_size = 1,
 		box_list = {
-			{addr_table = 0xA, id_ptr = 0xD, id_space = 0x04, type = PUSH_BOX},
-			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = VULNERABILITY_BOX},
-			--{addr_table = 0x6, id_ptr = 0xB, id_space = 0x04, type = WEAK_BOX},
-			{addr_table = 0x8, id_ptr = 0xC, id_space = 0x0C, type = ATTACK_BOX},
+			{addr_table = 0xA, id_ptr = 0xD, id_space = 0x04, type = "push"},
+			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = "vulnerability"},
+			--{addr_table = 0x6, id_ptr = 0xB, id_space = 0x04, type = "weak"}, --present but nonfunctional
+			{addr_table = 0x8, id_ptr = 0xC, id_space = 0x0C, type = "attack"},
 		},
+		throw_box_list = {
+			{param_offset = 0x6C, type = "throwable"},
+			{param_offset = 0x64, type = "throw"},
+		}
 	},
 	{
 		games = {"ssf2t"},
+		status_type = "normal",
 		address = {
 			player           = 0xFF844E,
 			projectile       = 0xFF97A2,
 			left_screen_edge = 0xFF8ED4,
-			game_phase       = 0xFF847F,
 			stage            = 0xFFE18B,
 		},
 		player_space       = 0x400,
 		box_parameter_size = 1,
 		box_list = {
-			{addr_table = 0x8, id_ptr = 0xD, id_space = 0x04, type = PUSH_BOX},
-			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x6, id_ptr = 0xC, id_space = 0x10, type = ATTACK_BOX},
+			{addr_table = 0x8, id_ptr = 0xD, id_space = 0x04, type = "push"},
+			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x6, id_ptr = 0xC, id_space = 0x10, type = "attack"},
 		},
+		throw_box_list = {
+			{param_offset = 0x6C, type = "throwable"},
+			{param_offset = 0x64, type = "throw"},
+		}
 	},
 	{
 		games = {"ssf2"},
+		status_type = "normal",
 		address = {
 			player           = 0xFF83CE,
 			projectile       = 0xFF96A2,
 			left_screen_edge = 0xFF8DD4,
-			game_phase       = 0xFF83FF,
 			stage            = 0xFFE08B,
 		},
 		player_space       = 0x400,
 		box_parameter_size = 1,
 		box_list = {
-			{addr_table = 0x8, id_ptr = 0xD, id_space = 0x04, type = PUSH_BOX},
-			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = VULNERABILITY_BOX},
-			{addr_table = 0x6, id_ptr = 0xC, id_space = 0x0C, type = ATTACK_BOX},
+			{addr_table = 0x8, id_ptr = 0xD, id_space = 0x04, type = "push"},
+			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x04, type = "vulnerability"},
+			{addr_table = 0x6, id_ptr = 0xC, id_space = 0x0C, type = "attack"},
 		},
+		throw_box_list = {
+			{param_offset = 0x6C, type = "throwable"},
+			{param_offset = 0x64, type = "throw"},
+		}
 	},
 	{
 		games = {"hsf2"},
+		status_type = "hsf2",
 		address = {
 			player           = 0xFF833C,
 			projectile       = 0xFF9554,
 			left_screen_edge = 0xFF8CC2,
-			game_phase       = 0xFF836D,
 			stage            = 0xFF8B65,
 		},
 		char_mode          = 0x32A,
 		player_space       = 0x400,
 		box_parameter_size = 2,
 		box_list = {
-			{addr_table = 0xA, id_ptr = 0xD, id_space = 0x08, type = PUSH_BOX},
-			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x08, type = VULNERABILITY_BOX},
-			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x08, type = VULNERABILITY_BOX},
-			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x08, type = VULNERABILITY_BOX},
-			{addr_table = 0x6, id_ptr = 0xB, id_space = 0x08, type = WEAK_BOX},
-			{addr_table = 0x8, id_ptr = 0xC, id_space = 0x14, type = ATTACK_BOX},
+			{addr_table = 0xA, id_ptr = 0xD, id_space = 0x08, type = "push"},
+			{addr_table = 0x0, id_ptr = 0x8, id_space = 0x08, type = "vulnerability"},
+			{addr_table = 0x2, id_ptr = 0x9, id_space = 0x08, type = "vulnerability"},
+			{addr_table = 0x4, id_ptr = 0xA, id_space = 0x08, type = "vulnerability"},
+			{addr_table = 0x6, id_ptr = 0xB, id_space = 0x08, type = "weak"},
+			{addr_table = 0x8, id_ptr = 0xC, id_space = 0x14, type = "attack"},
 		},
+		throw_box_list = {
+			{param_offset = 0x6C, type = "throwable"},
+			{param_offset = 0x64, type = "throw"},
+		}
 	},
 }
+
+for _,game in ipairs(profile) do
+	game.box_number = #game.box_list + #game.throw_box_list
+end
+
+for _,box in pairs(boxes) do
+	box.fill    = box.color * 0x100 + box.fill
+	box.outline = box.color * 0x100 + box.outline
+end
 
 local game, effective_delay
 local globals = {
@@ -187,10 +199,24 @@ local function adjust_delay(address)
 end
 
 
+local get_status = {
+	["normal"] = function()
+		if bit.band(memory.readword(0xFF8008), 0x08) > 0 then
+			return true
+		end
+	end,
+
+	["hsf2"] = function()
+		if memory.readword(0xFF8004) == 0x08 then
+			return true
+		end
+	end,
+}
+
 local function update_globals()
 	globals.left_screen_edge = memory.readword(game.address.left_screen_edge)
 	globals.top_screen_edge  = memory.readword(game.address.left_screen_edge + 0x4)
-	globals.game_phase       = memory.readword(game.address.game_phase)
+	globals.game_playing     = get_status[game.status_type]()
 end
 
 
@@ -208,7 +234,7 @@ local get_box_parameters = {
 	[1] = function(box)
 		box.hval   = memory.readbytesigned(box.address + 0)
 		box.hval2  = memory.readbyte(box.address + 5)
-		if box.hval2 >= 0x80 and box.type == ATTACK_BOX then
+		if box.hval2 >= 0x80 and box.type == "attack" then
 			box.hval = -box.hval2
 		end
 		box.vval   = memory.readbytesigned(box.address + 1)
@@ -225,25 +251,90 @@ local get_box_parameters = {
 }
 
 
+local process_box_type = {
+	["vulnerability"] = function(obj, box)
+	end,
+
+	["attack"] = function(obj, box)
+		if obj.projectile then
+			box.type = "proj. attack"
+		elseif memory.readbyte(obj.base + 0x03) == 0 then
+			return false
+		end
+	end,
+
+	["push"] = function(obj, box)
+		if obj.projectile then
+			box.type = "proj. vulnerability"
+		elseif not DRAW_PUSHBOXES then
+			return false
+		end
+	end,
+
+	["weak"] = function(obj, box)
+		if (game.char_mode and memory.readbyte(obj.base + game.char_mode) ~= 0x4)
+			or memory.readbyte(obj.animation_ptr + 0x15) ~= 2 then
+			return false
+		end
+	end,
+
+	["throw"] = function(obj, box)
+		get_box_parameters[2](box)
+		if box.hval == 0 and box.vval == 0 and box.hrad == 0 and box.vrad == 0 then
+			return false
+		end
+
+		for offset = 0,6,2 do
+			memory.writeword(box.address + offset, 0) --bad
+		end
+
+		box.hval   = obj.pos_x + box.hval * (obj.facing_dir == 1 and -1 or 1)
+		box.vval   = obj.pos_y - box.vval
+		box.left   = box.hval - box.hrad
+		box.right  = box.hval + box.hrad
+		box.top    = box.vval - box.vrad
+		box.bottom = box.vval + box.vrad
+	end,
+
+	["throwable"] = function(obj, box)
+		if not DRAW_THROWABLE_BOXES or
+			(memory.readbyte(obj.animation_ptr + 0x8) == 0 and
+			memory.readbyte(obj.animation_ptr + 0x9) == 0 and
+			memory.readbyte(obj.animation_ptr + 0xA) == 0) or
+			memory.readbyte(obj.base + 0x3) == 0x0E or
+			memory.readbyte(obj.base + 0x3) == 0x14 or
+			memory.readbyte(obj.base + 0x143) > 0 or
+			memory.readbyte(obj.base + 0x1BF) > 0 or
+			memory.readbyte(obj.base + 0x1A1) > 0 then
+			return false
+		elseif memory.readbyte(obj.base + 0x181) > 0 then
+			box.type = "air throwable"
+		end
+
+		box.hrad = memory.readword(box.address + 0)
+		box.vrad = memory.readword(box.address + 2)
+		box.hval = obj.pos_x
+		box.vval = obj.pos_y - box.vrad/2
+		box.left   = box.hval - box.hrad
+		box.right  = box.hval + box.hrad
+		box.top    = obj.pos_y - box.vrad
+		box.bottom = obj.pos_y
+	end,
+}
+
+
 local function define_box(obj, entry)
 	local box = {
 		type = game.box_list[entry].type,
 		id = memory.readbyte(obj.animation_ptr + game.box_list[entry].id_ptr),
 	}
 
-	if box.id == 0 then
-		return
-	elseif obj.projectile then
-		if box.type == ATTACK_BOX then
-			box.type = PROJ_ATTACK_BOX
-		else
-			box.type = PROJ_VULNERABILITY_BOX
-		end
+	if box.id == 0 or process_box_type[box.type](obj, box) == false then
+		return nil
 	end
 
 	local addr_table = obj.hitbox_ptr + memory.readwordsigned(obj.hitbox_ptr + game.box_list[entry].addr_table)
 	box.address = addr_table + box.id * game.box_list[entry].id_space
-
 	get_box_parameters[game.box_parameter_size](box)
 
 	box.hval   = obj.pos_x + box.hval * (obj.facing_dir == 1 and -1 or 1)
@@ -257,6 +348,20 @@ local function define_box(obj, entry)
 end
 
 
+local function define_throw_box(obj, entry)
+	local box = {
+		type = game.throw_box_list[entry].type,
+		address = obj.base + game.throw_box_list[entry].param_offset,
+	}
+
+	if process_box_type[box.type](obj, box) == false then
+		return nil
+	end
+
+	return box
+end
+
+
 local function update_game_object(obj)
 	obj.facing_dir    = memory.readbyte(obj.base + 0x12)
 	obj.pos_x         = get_x(memory.readwordsigned(obj.base + 0x06))
@@ -265,7 +370,7 @@ local function update_game_object(obj)
 	obj.hitbox_ptr    = memory.readdword(obj.base + 0x34)
 
 	for entry in ipairs(game.box_list) do
-		obj[entry] = define_box(obj, entry, animation_ptr, hitbox_ptr)
+		table.insert(obj, define_box(obj, entry))
 	end
 end
 
@@ -285,8 +390,6 @@ local function read_projectiles()
 	for i = 1, MAX_BONUS_OBJECTS do
 		local obj = {base = game.address.projectile + (MAX_GAME_PROJECTILES + i-1) * 0xC0}
 		if bit.band(0xff00, memory.readword(obj.base)) == 0x0100 then
-			obj.no_attack = memory.readbyte(obj.base + 0x03) == 0
-			obj.projectile = true
 			update_game_object(obj)
 			table.insert(current_projectiles, obj)
 		end
@@ -303,23 +406,29 @@ local function update_sf2_hitboxes()
 	effective_delay = adjust_delay(game.address.stage)
 	update_globals()
 
-	for p = 1, NUMBER_OF_PLAYERS do
-		player[p] = {base = game.address.player + (p-1) * game.player_space}
-		if memory.readword(player[p].base) > 0x0100 then
-			player[p].no_weak = game.char_mode and memory.readbyte(player[p].base + game.char_mode) ~= 0x4
-			update_game_object(player[p])
-		end
-	end
-
 	for f = 1, effective_delay do
+		frame_buffer[f].status = frame_buffer[f+1].status
 		for p = 1, NUMBER_OF_PLAYERS do
 			frame_buffer[f][player][p] = copytable(frame_buffer[f+1][player][p])
 		end
 		frame_buffer[f][projectiles] = copytable(frame_buffer[f+1][projectiles])
 	end
 
+	frame_buffer[effective_delay+1].status = globals.game_playing
 	for p = 1, NUMBER_OF_PLAYERS do
-		frame_buffer[effective_delay+1][player][p] = copytable(player[p])
+		player[p] = {base = game.address.player + (p-1) * game.player_space}
+		if memory.readword(player[p].base) > 0x0100 then
+			update_game_object(player[p])
+		end
+		frame_buffer[effective_delay+1][player][p] = player[p]
+
+		local prev_frame = frame_buffer[effective_delay][player][p]
+		if prev_frame and prev_frame.pos_x then
+			for entry in ipairs(game.throw_box_list) do
+				table.insert(prev_frame, define_throw_box(prev_frame, entry))
+			end
+		end
+
 	end
 	frame_buffer[effective_delay+1][projectiles] = read_projectiles()
 end
@@ -334,18 +443,13 @@ end)
 
 local function draw_hitbox(obj, entry)
 	local hb = obj[entry]
-	if (not DRAW_PUSHBOXES and hb.type == PUSH_BOX) or
-		(hb.type == PROJ_ATTACK_BOX and obj.no_attack) or
-		(hb.type == WEAK_BOX and (obj.no_weak or memory.readbyte(obj.animation_ptr + 0x15) ~= 2)) then
-		return
-	end
 
 	if DRAW_MINI_AXIS then
-		gui.drawline(hb.hval, hb.vval-MINI_AXIS_SIZE, hb.hval, hb.vval+MINI_AXIS_SIZE, outline[hb.type])
-		gui.drawline(hb.hval-MINI_AXIS_SIZE, hb.vval, hb.hval+MINI_AXIS_SIZE, hb.vval, outline[hb.type])
+		gui.drawline(hb.hval, hb.vval-MINI_AXIS_SIZE, hb.hval, hb.vval+MINI_AXIS_SIZE, boxes[hb.type].outline)
+		gui.drawline(hb.hval-MINI_AXIS_SIZE, hb.vval, hb.hval+MINI_AXIS_SIZE, hb.vval, boxes[hb.type].outline)
 	end
 
-	gui.box(hb.left, hb.top, hb.right, hb.bottom, fill[hb.type], outline[hb.type])
+	gui.box(hb.left, hb.top, hb.right, hb.bottom, boxes[hb.type].fill, boxes[hb.type].outline)
 end
 
 
@@ -362,7 +466,7 @@ end
 
 local function render_sf2_hitboxes()
 	gui.clearuncommitted()
-	if not game or globals.game_phase == GAME_PHASE_NOT_PLAYING then
+	if not game or not frame_buffer[1].status then
 		return
 	end
 
@@ -370,7 +474,7 @@ local function render_sf2_hitboxes()
 		gui.box(0, 0, emu.screenwidth(), emu.screenheight(), BLANK_COLOR)
 	end
 
-	for entry in ipairs(game.box_list) do
+	for entry = 1, game.box_number do
 		for i in ipairs(frame_buffer[1][projectiles]) do
 			local obj = frame_buffer[1][projectiles][i]
 			if obj[entry] then
@@ -430,6 +534,13 @@ input.registerhotkey(4, function()
 	DRAW_PUSHBOXES = not DRAW_PUSHBOXES
 	render_sf2_hitboxes()
 	print((DRAW_PUSHBOXES and "showing" or "hiding") .. " pushboxes")
+end)
+
+
+input.registerhotkey(5, function()
+	DRAW_THROWABLE_BOXES = not DRAW_THROWABLE_BOXES
+	render_sf2_hitboxes()
+	print((DRAW_THROWABLE_BOXES and "showing" or "hiding") .. " throwable boxes")
 end)
 
 
